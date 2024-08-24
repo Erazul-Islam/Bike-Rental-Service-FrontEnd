6f73d7a01a5a39ae276c977d88a6f58cbf4afb3f
Erazul Islam <erazul89@gmail.com>
2024-08-24 19:13:09 +0600

    initial setup and created the navbar

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Nav from "./component/navbar/Nav";
+
+const App = () => {
+  return (
+    <div className="dark text-foreground bg-background h-screen">
+      <Nav />
+    </div>
+  );
+};
+
+export default App;
