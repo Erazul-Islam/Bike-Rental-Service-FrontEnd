24818ccd480ec7702abc0a1f4c9d2657ab28e100
Erazul Islam <erazul89@gmail.com>
2024-08-25 15:20:34 +0600

    create the redux part

--- src/redux/feature/productApi/ProductApi.tsx
+++ src/redux/feature/productApi/ProductApi.tsx
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const productApi = createApi({
     reducerPath : 'productsApi',
-    baseQuery: fetchBaseQuery({baseUrl:'http://localhost:5000'}),
+    baseQuery: fetchBaseQuery({baseUrl:'http://localhost:5000/api'}),
     endpoints: (builder) => ({
         
     })
